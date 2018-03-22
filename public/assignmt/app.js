4da9b1f1c6ab5e5090bbe106b825d15b6b431785
mounika <uppala.mounika13@gmail.com>
2018-03-22 15:01:18 -0400

    initial commit

---
+++ public/assignmt/app.js
@@ -0,0 +1,27 @@
+
+
+var app = angular.module("WamApp", [] );
+
+app.controller("loginController", loginController);
+
+function loginController($scope){
+
+    users = [
+        {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder" },
+        {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley" },
+        {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia" },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi" }
+    ];
+
+    $scope.login = function (user){
+
+        for(var u  in  users) {
+            var _user = users[u];
+            if (_user.username === user.username && _user.password === user.password)
+                $scope.welcomeUser = _user;
+        }
+    }
+}
+
+
+
